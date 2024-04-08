46536bafee37881c4e0eaad3d5afdf3043bc42e4
Adi123XD <adarshagarwal932@gmail.com>
2024-04-08 17:16:30 +0530

    Results page moved

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Markdown from 'react-markdown';
-import '../Navbar.css';
+import './Results.css';
 
 const Results = () => {
     const location = useLocation();
